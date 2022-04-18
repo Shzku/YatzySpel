a213b0401b1c2b0d6cb77cc2c95dabcfc81087ac
Seebus <19seth@stockholmscience.se>
2022-04-19 01:06:48 +0200

    oops can't forget these

---
+++ src/router/index.ts
@@ -0,0 +1,21 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import StartView from '@/views/StartView.vue';
+import GameView from '@/views/GameView.vue';
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'start',
+      component: StartView
+    },
+    {
+      path: '/yatzy',
+      name: 'yatzy',
+      component: GameView
+    }
+  ]
+})
+
+export default router
